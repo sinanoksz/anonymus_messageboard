5b694db9a345fc2c7827a0736555b273a70d6e72
Sinan Öksüz <98950852+sinanoksz@users.noreply.github.com>
2025-01-27 18:05:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,49 @@
 'use strict';
 require('dotenv').config();
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
 
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// helmet 
+const helmet = require("helmet")
+
+app.use(
+  helmet.frameguard({
+    action: "sameorigin",
+  })
+);
+
+app.use(helmet.dnsPrefetchControl({
+  allow: false
+}))
+
+app.use(helmet.referrerPolicy({
+  policy: ["same-origin"],
+}))
+
+// to connect mongoose
+const mongoose = require('mongoose')
+mongoose.connect(process.env['DB'])
+  .then(() => {
+    console.log('Mongoose connected')
+  })
+  .catch((err) => {
+    console.log('error', err)
+  })
 
 //Sample front-end
 app.route('/b/:board/')
@@ -40,7 +68,7 @@ fccTestingRoutes(app);
 apiRoutes(app);
 
 //404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
@@ -49,12 +77,12 @@ app.use(function(req, res, next) {
 //Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
     setTimeout(function () {
       try {
         runner.run();
-      } catch(e) {
+      } catch (e) {
         console.log('Tests are not valid:');
         console.error(e);
       }
